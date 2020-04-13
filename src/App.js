7bfcd05e9bc4317cdb6c77128f78d731ef3df983
ragilhadi <ragilprasetyo310@gmail.com>
2020-04-13 03:40:30 -0700

    First Commit and top nav

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopNav from './components/header/TopNav';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav />
+    </div>
+  );
+}
+
+export default App;
