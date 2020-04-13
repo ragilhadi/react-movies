53c37d5b74c5f21ace9dc5139d080d550504cad1
ragilhadi <ragilprasetyo310@gmail.com>
2020-04-13 03:56:02 -0700

    Add Carousel and Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import TopNav from './components/header/TopNav';
+import ImageSlide from './components/imageSlider/ImageSlide';
+import ItemCard from './components/itemCard/ItemCard';
 
 function App() {
   return (
     <div className="App">
       <TopNav />
+      <ImageSlide />
+      <ItemCard />
     </div>
   );
 }
---
+++ src/components/imageSlider/ImageSlide.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Carousel } from "antd";
+import 'antd/dist/antd.css';
+import './ImageSlide.scss'
+
+const ImageSlide = (props) => {
+  return (
+    <Carousel autoplay>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+    </Carousel>
+  );
+};
+
+export default ImageSlide;
