9bd52989dd44d2b0c902d6f61b9b51f0feeef7a5
ragilhadi <ragilprasetyo310@gmail.com>
2020-04-18 04:09:21 +0700

    Updates

---
+++ precache-manifest.c2953fc2f21b334aa395fd036149abac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7608039bbdc56dc53f32fabf3d52695",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "b6687c37b8f6c1d9d57d",
+    "url": "/react-movies/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "827ff9cd218f0c57eed2",
+    "url": "/react-movies/static/css/main.5c3907ba.chunk.css"
+  },
+  {
+    "revision": "b6687c37b8f6c1d9d57d",
+    "url": "/react-movies/static/js/2.56eb912b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-movies/static/js/2.56eb912b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827ff9cd218f0c57eed2",
+    "url": "/react-movies/static/js/main.34835b2a.chunk.js"
+  },
+  {
+    "revision": "193ca6e6058686f3aa5e",
+    "url": "/react-movies/static/js/runtime-main.704bfe2d.js"
+  },
+  {
+    "revision": "56fd61eec148690af48f876dfd5c8718",
+    "url": "/react-movies/static/media/logo_horizontal.56fd61ee.svg"
+  }
+]);
