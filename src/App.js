02c5dc3a68dcdec5f08fe6a5658a6a225c6a1495
ragilhadi <ragilprasetyo310@gmail.com>
2020-04-18 04:40:13 +0700

    fix problem router again

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import Movie from "./View/Movie/Movie";
 import NotFound from "./View/NotFound/NotFound";
 
 class App extends Component {
+
+  APP_URL = 'http://ragilhadi.github.io/react-movies'
+
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={`${process.env.APP_URL}/`}>
         <React.Fragment>
           <TopNav />
           <Switch>
